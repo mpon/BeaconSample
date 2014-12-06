2ce021f85561d5af03007d1bab2778967f078c21
Masato Oshima <qqw75pn9@gmail.com>
2014-12-06 11:14:08 +0900

    Initial Commit

---
+++ BeaconSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconSample
+//
+//  Created by Masato Oshima on 2014/12/06.
+//  Copyright (c) 2014年 Masato Oshima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
